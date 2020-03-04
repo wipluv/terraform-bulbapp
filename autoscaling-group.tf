5ebaba542b569a80524ccd1975982b162cf14119
wipluv <60206056+wipluv@users.noreply.github.com>
2020-03-03 17:40:30 -0800

    Update autoscaling-group.tf

--- autoscaling-group.tf
+++ autoscaling-group.tf
@@ -7,6 +7,6 @@ resource "aws_autoscaling_group" "default" {
   desired_capacity          = 1
   force_delete              = true
   launch_configuration      = "${aws_launch_configuration.default.name}"
-  vpc_zone_identifier       = ["subnet-987c67b6", "subnet-987c67b6"]
-#  target_group_arns         = ["${aws_lb_target_group.default.arn}"]
+  vpc_zone_identifier       = ["subnet-bd223be1", "subnet-987c67b6"] 
+  target_group_arns         = ["${aws_lb_target_group.default.arn}"]
 }
