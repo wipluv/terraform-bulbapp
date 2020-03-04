73b49d92a24524960e8b2f23cf8dd750a5141137
wipluv <60206056+wipluv@users.noreply.github.com>
2020-03-03 20:10:31 -0800

    Update sgs.tf

--- sgs.tf
+++ sgs.tf
@@ -1,18 +1,22 @@
-resource "aws_security_group" "default_loadbalancer" {
-  name        = "terraform-ecs-bulbapp-loadbalancer"
+resource "aws_security_group" "default" {
+  name        = "terraform-ecs-bulbapp"
   description = "Allow traffic"
   vpc_id      = "vpc-ffb59c85"
-    ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }  ingress {
+  
+  ingress {
+     from_port   = 22
+     to_port     = 22
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+      
+  }  
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }  egress {
+    cidr_blocks = ["0.0.0.0/0"] 
+  } 
+  egress {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
