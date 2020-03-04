b3abd90c79ee95487aea1e076cbc4db6e15b618e
wipluv <bipsharma111@gmail.com>
2020-03-03 16:07:55 -0800

    bhayo

---
+++ task-defination.tf
@@ -0,0 +1,23 @@
+data "aws_ecs_task_definition" "default" {
+  task_definition = "${aws_ecs_task_definition.default.family}"
+}
+resource "aws_ecs_task_definition" "default" {
+    family                = "bulbapp"
+    container_definitions = <<DEFINITION
+[
+  {
+    "name": "bulbapp",
+    "image": "wipluv/helloapp-bulbapp:v1",
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "hostPort": 80
+      }
+    ],
+    "memory": 512,
+    "cpu": 1024
+  },
+]
+DEFINITION
+}
