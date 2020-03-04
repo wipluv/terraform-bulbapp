488f81c717144a8a362280597919c4db42c83e5a
wipluv <60206056+wipluv@users.noreply.github.com>
2020-03-03 16:32:45 -0800

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -19,5 +19,5 @@ resource "aws_iam_role_policy_attachment" "default" {
 
 resource "aws_iam_instance_profile" "default" {
     name = "terraform-ecs-bulbapp"
-    roles = ["${aws_iam_role.default.id}"]
+    role = ["${aws_iam_role.default.id}"]
 }
