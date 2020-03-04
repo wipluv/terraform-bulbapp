2840a3dd8707ed256b2d6946f20b203209fd817e
wipluv <60206056+wipluv@users.noreply.github.com>
2020-03-03 17:39:20 -0800

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "default" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = ["${aws_security_group.default_lb.id}"]
-  subnets            = ["subnet-fb8428b6", "subnet-bd223be1"] 
+  subnets            = ["subnet-bd223be1", "subnet-987c67b6"] 
   enable_deletion_protection = false
   tags = {
     Environment = "dev"
