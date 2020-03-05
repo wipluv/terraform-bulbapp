b6abbe5236200d37cfe32caa2f30911eced607cf
wipluv <60206056+wipluv@users.noreply.github.com>
2020-03-04 17:42:23 -0800

    Update sgs.tf

--- sgs.tf
+++ sgs.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "default" {
   egress {
     from_port       = 0
     to_port         = 0
-    protocol        = "-1"
+    protocol        = "tcp"
     cidr_blocks     = ["0.0.0.0/0"]
   }
 }
