68103be2800d48738da79331c16de686768f1667
wipluv <60206056+wipluv@users.noreply.github.com>
2020-03-04 17:44:06 -0800

    Update service.tf

--- service.tf
+++ service.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_service" "default" {
   	iam_role        = "${aws_iam_role.ecs-service-role.name}"
   	cluster         = "${aws_ecs_cluster.default.id}"
   	task_definition = "${aws_ecs_task_definition.default.family}:${max("${aws_ecs_task_definition.default.revision}", 0)}"
-  	desired_count   = 3 
+  	desired_count   = 1
         load_balancer {
     		target_group_arn  = "${aws_lb_target_group.default.arn}"
     		container_port    = 80
